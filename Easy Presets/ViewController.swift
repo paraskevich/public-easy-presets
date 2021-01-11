b853643b58fe35742b4bc61b049b23a35f2dce52
paraskevich <ilya.paraskevich@gmail.com>
2021-01-11 21:10:39 +0300

    Initial Commit

---
+++ Easy Presets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Presets
+//
+//  Created by ILYA Paraskevich on 11.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Easy PresetsTests/Easy_PresetsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Easy_PresetsTests.swift
+//  Easy PresetsTests
+//
+//  Created by ILYA Paraskevich on 11.01.21.
+//
+
+import XCTest
+@testable import Easy_Presets
+
+class Easy_PresetsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
